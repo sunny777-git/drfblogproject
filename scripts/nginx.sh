0f832f9ade0065898290aaa5a9297d75a26c8051
Sunny Kumar <sunny777.official@gmail.com>
2023-04-03 08:46:41 +0530

    files added

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -2,11 +2,11 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 #!/bin/bash
-sudo rm -f /etc/nginx/sites-enabled/blog 
+# sudo rm -f /etc/nginx/sites-enabled/blog 
 sudo cp /home/ubuntu/blogprojectdrf/nginx/nginx.conf /etc/nginx/sites-available/blog
 sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
-sudo nginx -t
+# sudo nginx -t
 sudo gpasswd -a www-data ubuntu
 sudo systemctl restart nginx
 
